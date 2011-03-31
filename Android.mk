d216827414f27819e04a9e6a3b2814e19169edad
richiedaze <richiedaze@gmail.com>
2011-03-31 01:03:26 -0400

    first commit

---
+++ Android.mk
@@ -0,0 +1,14 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_PACKAGE_NAME := Espresso
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+# LOCAL_AAPT_FLAGS := -x 10
+
+LOCAL_AAPT_FLAGS := -c hdpi -x 10
+
+include $(BUILD_PACKAGE)
---
+++ src/richiedaze/mt4g/theme/dummy.java
@@ -0,0 +1,8 @@
+package richiedaze.mt4g.theme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class dummy extends Activity {
+    
+}
